fc5a6688011d31a6da8d0b9fa43044cbc120232f
jakub-bartal <kuboo50@gmail.com>
2019-03-14 17:19:27 +0100

    [TFIP-57] - Add meeting time view page to layout

---
+++ resources/pages/meeting-times.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-layout column justify-center align-end>
+    <v-btn @click="createNewSitDown" color="primary" dark>
+      <i class="material-icons">add</i>
+      NOVÝ SITDOWN
+    </v-btn>
+
+    <v-card class='elevation-1 fullscreen'>
+      <v-layout align-center justify-end>
+        content
+      </v-layout>
+    </v-card>
+  </v-layout>
+</template>
+
+<script>
+  export default {
+    computed: {
+    },
+    data () {
+      return {
+      };
+    },
+    methods: {
+      createNewSitDown () {
+      },
+    },
+  };
+</script>
