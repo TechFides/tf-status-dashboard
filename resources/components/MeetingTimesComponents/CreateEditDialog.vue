81eded9de29b820540091bf7bcc79a08e3f22453
jakub-bartal <kuboo50@gmail.com>
2019-03-18 17:05:11 +0100

    [TFIP-57] - Add CreateEditDialog and table component

---
+++ resources/components/MeetingTimesComponents/CreateEditDialog.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-dialog v-model="isOpen" max-width="550px">
+    <v-card>
+      <v-card-title>
+        <span class="headline">{{ title }}</span>
+      </v-card-title>
+
+      <v-card-text>
+        <v-container grid-list-md>
+          <v-layout wrap column>
+            <v-flex xs12 sm6 md4>
+              <v-text-field
+                :rules="[]"
+                v-bind:value="dialogData.name"
+                v-on:input="$emit('nameChange', $event)"
+                label="Name"
+              ></v-text-field>
+            </v-flex>
+            <v-flex xs12 sm6 md4>
+              <v-select
+                :items="weekDays"
+                v-bind:value="dialogData.weekDay"
+                v-on:input="$emit('weekDayChange', $event)"
+                label="Select week day"
+              >
+              </v-select>
+            </v-flex>
+            <v-flex xs12 sm6 md4 class="time-picker">
+              <v-time-picker
+                landscape
+                v-bind:value="dialogData.hour"
+                v-on:input="$emit('hourChange', $event)"
+              ></v-time-picker>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-card-text>
+
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="blue darken-1" flat @click="close">Zrušit</v-btn>
+        <v-btn color="blue darken-1" flat @click="submit">Uložit</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+  export default {
+    name: 'CreateEditDialog',
+    props: ['isOpen', 'close', 'submit', 'dialogData', 'title'],
+    data () {
+      return {
+        weekDays: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+      };
+    },
+  };
+</script>
+
+<style>
+  .time-picker {
+    display: flex;
+    justify-content: center;
+    margin-top: 20px;
+  }
+</style>
---
+++ resources/components/MeetingTimesComponents/Table.vue
@@ -0,0 +1,78 @@
+<template>
+  <v-data-table
+    :headers='headers'
+    :items='meetingTimes'
+    item-key="id"
+    hide-actions
+    fill-height
+    class='elevation-1 fullscreen'
+  >
+    <template slot='items' slot-scope='{ item }'>
+      <td class='text-xs-center element'>{{ item.id }}</td>
+      <td class='text-xs-center element'>{{ item.hour }}</td>
+      <td class='text-xs-center element'>{{ item.week_day }}</td>
+      <td class='text-xs-center element'>{{ item.projects }}</td>
+      <td class="justify-center layout px-0">
+        <v-icon
+          small
+          class="mr-2"
+          @click="openEditDialog(item)"
+        >
+          edit
+        </v-icon>
+        <v-icon
+          small
+          @click="deleteItem(item.id)"
+        >
+          delete
+        </v-icon>
+      </td>
+
+    </template>
+  </v-data-table>
+</template>
+
+<script>
+  export default {
+    name: 'MeetingTimesTable',
+    props: ['meetingTimes', 'deleteItem', 'openEditDialog'],
+    computed: {
+      headers: function () {
+        return [
+          {
+            text: 'Název',
+            align: 'center',
+            sortable: false,
+          },
+          {
+            text: 'Hodina',
+            align: 'center',
+            sortable: false,
+          },
+          {
+            text: 'Den v týdnu',
+            align: 'center',
+            sortable: false,
+          },
+          {
+            text: 'Projekty',
+            align: 'center',
+            sortable: false,
+          },
+          {
+            text: 'Akce',
+            align: 'center',
+            sortable: false,
+          },
+        ];
+      },
+    },
+  };
+</script>
+
+<style scoped>
+  .fullscreen {
+    width: 100%;
+    height: 100%;
+  }
+</style>
