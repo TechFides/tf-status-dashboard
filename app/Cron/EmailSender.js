e6a73db77e5a77de70587464aa23162c5dd9a493
David Smejkal <xsmejk10@node.mendelu.cz>
2019-04-18 17:20:40 +0200

    create template for sending email

--- app/Cron/EmailSender.js
+++ app/Cron/EmailSender.js
@@ -2,40 +2,21 @@ const AWS = require('aws-sdk');
 
 AWS.config.update({region: 'eu-west-1'})
 
-const params = {
-  Destination: { /* required */
-    CcAddresses: [
-      'david.smejkal@techfides.cz'
-    ],
-    ToAddresses: [
-      'david.smejkal@techfides.cz'
-    ]
-  },
-  Message: {
-    Body: {
-      Html: {
-        Charset: 'UTF-8',
-        Data: 'HTML_FORMAT_BODY'
-      },
-      Text: {
-        Charset: 'UTF-8',
-        Data: 'TEXT_FORMAT_BODY'
-      }
-    },
-    Subject: {
-      Charset: 'UTF-8',
-      Data: 'Test email'
-    }
-  },
-  Source: 'david.smejkal@techfides.cz',
-  ReplyToAddresses: [
-    'david.smejkal@techfides.cz'
-  ]
-}
+let params = {};
+let templateData = {};
+templateData.userName='David';
+let destination = {
+  'ToAddresses': ['david.smejkal@techfides.cz']
+};
+
+params.TemplateData = JSON.stringify(templateData);
+params.Source = 'david.smejkal@techfides.cz';
+params.Destination = destination;
+params.Template = 'feedbackTemplate';
 
 async function sendPromise () {
   try {
-    let sendPromise = await new AWS.SES({apiVersion: '2010-12-01'}).sendEmail(params).promise();
+    let promise = await new AWS.SES({apiVersion: '2010-12-01'}).sendTemplatedEmail(params).promise();
     console.log('done');
   } catch(err) {
     console.log(err.toString())
