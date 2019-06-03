a6d7aa46bfffa3e1929eccffa8f1af43a5588e10
David Smejkal <xsmejk10@node.mendelu.cz>
2019-06-03 08:43:54 +0200

    tfip-43 lint fix

--- app/Cron/EmailSender.js
+++ app/Cron/EmailSender.js
@@ -1,12 +1,12 @@
 const AWS = require('aws-sdk');
 
-AWS.config.update({region: 'eu-west-1'})
+AWS.config.update({region: 'eu-west-1'});
 
 let params = {};
 let templateData = {};
 templateData.userName = 'David';
 let destination = {
-  'ToAddresses': ['david.smejkal@techfides.cz']
+  'ToAddresses': ['david.smejkal@techfides.cz'],
 };
 
 params.TemplateData = JSON.stringify(templateData);
@@ -18,8 +18,8 @@ async function sendPromise () {
   try {
     let promise = await new AWS.SES({apiVersion: '2010-12-01'}).sendTemplatedEmail(params).promise();
     console.log('done');
-  } catch(err) {
-    console.log(err.toString())
+  } catch (err) {
+    console.log(err.toString());
   }
 }
 
