84ba3ee33e82a584f4ab9eff4fa143a6aea03ade
David Smejkal <xsmejk10@node.mendelu.cz>
2020-03-02 08:57:53 +0100

    TFIP-222-fixed migration for adding enum data

--- database/migrations/1582707838875_standup_project_rating_enums_schema.js
+++ database/migrations/1582707838875_standup_project_rating_enums_schema.js
@@ -5,44 +5,7 @@ const Schema = use('Schema')
 
 class StandupProjectRatingEnumsSchema extends Schema {
   async up () {
-    await use('App/Models/StandupProjectRatingEnum').truncate();
     const enumData = [
-      {
-        id: 0,
-        name: 'HIATUS',
-        description: 'Nothing was done',
-        value: 0,
-      },
-      {
-        id: 1,
-        name: 'FAIL',
-        description: 'Error with big negative impact on our side',
-        value: -100,
-      },
-      {
-        id: 2,
-        name: 'BAD',
-        description: 'Below average',
-        value: 30,
-      },
-      {
-        id: 3,
-        name: 'STANDARD',
-        description: 'Average',
-        value: 60,
-      },
-      {
-        id: 4,
-        name: 'GOOD',
-        description: 'Doing better than expected',
-        value: 90,
-      },
-      {
-        id: 5,
-        name: 'AMAZING',
-        description: 'Great positive impact from our side',
-        value: 120,
-      },
       {
         id: 6,
         name: 'STANDARD_v2',
@@ -55,53 +18,18 @@ class StandupProjectRatingEnumsSchema extends Schema {
         description: 'Doing better than expected',
         value: 80,
       },
+      {
+        id: 8,
+        name: 'DEFAULT',
+        description: 'Default value',
+        value: 0,
+      },
     ];
 
     await use('App/Models/StandupProjectRatingEnum').createMany(enumData);
   }
 
   async down () {
-    await use('App/Models/StandupProjectRatingEnum').truncate();
-    const enumData = [
-      {
-        id: 0,
-        name: 'HIATUS',
-        description: 'Nothing was done',
-        value: 0,
-      },
-      {
-        id: 1,
-        name: 'FAIL',
-        description: 'Error with big negative impact on our side',
-        value: -100,
-      },
-      {
-        id: 2,
-        name: 'BAD',
-        description: 'Below average',
-        value: 30,
-      },
-      {
-        id: 3,
-        name: 'STANDARD',
-        description: 'Average',
-        value: 60,
-      },
-      {
-        id: 4,
-        name: 'GOOD',
-        description: 'Doing better than expected',
-        value: 90,
-      },
-      {
-        id: 5,
-        name: 'AMAZING',
-        description: 'Great positive impact from our side',
-        value: 120,
-      },
-    ];
-
-    await use('App/Models/StandupProjectRatingEnum').createMany(enumData);
   }
 }
 
