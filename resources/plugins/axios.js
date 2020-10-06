5ecb451b5dba21f7d18b01c55b10bcc2d808ade6
Toňa <antonina.nesmelova@techfides.cz>
2020-10-06 11:28:41 +0200

    [TFH-70] Fix maximum call stack size exceeded error

--- resources/plugins/axios.js
+++
@@ -1,9 +0,0 @@
-export default function ({ $axios, app, redirect }) {
-  $axios.onError(error => {
-    app.$notifier.showMessage({ message: error.message, color: 'red' });
-  });
-  $axios.onResponse(response => {
-    app.$notifier.showMessage({ message: 'here', color: 'green' });
-    return response;
-  });
-}
